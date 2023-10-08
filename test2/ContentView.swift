f11e18c74f0ea6e3d0230550eac1e08f0cda6485
renads22 <roro1x1x@gmail.com>
2023-10-08 10:49:19 +0300

    Initial Commit

---
+++ test2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test2
+//
+//  Created by Renad alkhathami on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Renad alkhathami on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
